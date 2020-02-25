fe47183aa9b595f37667f7db7881144da45ab5e8
Michał Góra <michalgorazs3@gmail.com>
2020-02-25 20:54:11 +0100

    Update Controllers, Models, Views, Partial Views

--- WebAppShop/App_Start/RouteConfig.cs
+++ WebAppShop/App_Start/RouteConfig.cs
@@ -18,6 +18,18 @@ namespace WebAppShop
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            #region rozszerzenia routing
+            ////Routing umieszczony nad kontrolerem
+            //routes.MapMvcAttributeRoutes();
+
+            //routes.MapRoute(
+            //    "MoviesByReleaseDate",
+            //    "movies/released/{year}/{month}",
+            //    new { controller = "Movies", action = "ByReleaseDate" },
+            //    new { year = @"\d{4}", month = @"\d{2}"}
+            //);
+            #endregion
         }
     }
 }
--- WebAppShop/Controllers/MoviesController.cs
+++ WebAppShop/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebAppShop.Models;
+using WebAppShop.ViewModels;
 
 namespace WebAppShop.Controllers
 {
@@ -13,7 +14,53 @@ namespace WebAppShop.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Obcy" };
-            return View(movie);
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer1"},
+                new Customer { Name = "Customer2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
         }
+
+        #region rozszerzenia kontrolera
+        //[Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        //public ActionResult ByReleaseDate(int year, int month)
+        //{
+        //    return Content(year + "/" + month);
+        //}
+        //public ActionResult Random()
+        //{
+        //var movie = new Movie() { Name = "Obcy" };
+        //return View(movie);
+        //return new ViewResult();
+        //return Content("Example");
+        //return HttpNotFound();
+        //return new EmptyResult();
+        //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name"});
+        //}
+
+        //public ActionResult Edit(int id)
+        //{
+        //    return Content("id= " + id);
+        //}
+
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
+
+        //    if (String.IsNullOrWhiteSpace(sortBy))
+        //        sortBy = "Name";
+
+        //    return Content(String.Format("pageIndex = {0} & sortBy = {1}", pageIndex, sortBy));
+        //}
+        #endregion
     }
 }
